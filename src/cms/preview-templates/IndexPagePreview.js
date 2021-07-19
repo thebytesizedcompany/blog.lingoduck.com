1a1aa546c507e104bb0538a4d26009eb28864822
Meek at SendPlaid <meek@sendplaid.com>
2021-07-19 08:01:49 -0700

    make blog standalone

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,32 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import { BlogPostTemplate } from '../../templates/blog-post'
 
-const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  if (data) {
-    return (
-      <IndexPageTemplate
-        image={getAsset(data.image)}
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
-    )
-  } else {
-    return <div>Loading...</div>
-  }
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  const tags = entry.getIn(['data', 'tags'])
+  return (
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={tags && tags.toJS()}
+      title={entry.getIn(['data', 'title'])}
+    />
+  )
 }
 
-IndexPagePreview.propTypes = {
+BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  getAsset: PropTypes.func,
+  widgetFor: PropTypes.func,
 }
 
-export default IndexPagePreview
+export default BlogPostPreview
